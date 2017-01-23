3c572cb39e0dc40940885c6329880a2d84cfffcd
icedike <icedike@gmail.com>
2017-01-23 19:32:41 +0800

    first commit

---
+++ Test_Travis_CI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Travis_CI
+//
+//  Created by LIN TINGMIN on 23/01/2017.
+//  Copyright © 2017 MarkRobotDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
